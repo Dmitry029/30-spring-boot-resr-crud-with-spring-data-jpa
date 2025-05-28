f1e4832d5576bab9766034889b69b3f97b41a330
Dmitry029 <kdp.029029@cmail.com>
2025-05-28 22:47:31 +0300

    add spring data JPA

--- src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,48 @@
 package com.luv2code.cruddemo.service;
 
-import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.dao.EmployeeRepository;
 import com.luv2code.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeDAO) {
+        this.employeeRepository = employeeDAO;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        }
+        else {
+            throw  new RuntimeException("Did not find employee id - " + id);
+        }
+        return theEmployee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    @Transactional
     @Override
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
